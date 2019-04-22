fbd6213f7d388888a0155c48accc9d07016c12fd
razilein <soul_reaverfan@hotmail.de>
2019-04-22 11:34:27 +0200

    Suche bei Produkten - Wenn EAN gefüllt nur nach EAN suchen

--- src/main/java/de/sg/computerinsel/tools/inventar/dao/ProduktRepository.java
+++ src/main/java/de/sg/computerinsel/tools/inventar/dao/ProduktRepository.java
@@ -13,33 +13,18 @@ public interface ProduktRepository extends CrudRepository<Produkt, Integer> {
 
     Page<Produkt> findAll(Pageable pagination);
 
-    Page<Produkt> findByBezeichnungLikeAndGruppeKategorieIdAndGruppeIdAndEan(String bezeichnung, Integer kategorieId, Integer gruppeId,
-            String ean, Pageable pagination);
-
     Page<Produkt> findByBezeichnungLikeAndGruppeKategorieIdAndGruppeId(String bezeichnung, Integer kategorieId, Integer gruppeId,
             Pageable pagination);
 
-    Page<Produkt> findByBezeichnungLikeAndGruppeKategorieIdAndEan(String bezeichnung, Integer kategorieId, String ean, Pageable pagination);
-
     Page<Produkt> findByBezeichnungLikeAndGruppeKategorieId(String bezeichnung, Integer kategorieId, Pageable pagination);
-
-    Page<Produkt> findByBezeichnungLikeAndGruppeIdAndEan(String bezeichnung, Integer gruppeId, String ean, Pageable pagination);
 
     Page<Produkt> findByBezeichnungLikeAndGruppeId(String bezeichnung, Integer gruppeId, Pageable pagination);
 
-    Page<Produkt> findByBezeichnungLikeAndEan(String bezeichnung, String ean, Pageable pagination);
-
     Page<Produkt> findByBezeichnungLike(String bezeichnung, Pageable pagination);
-
-    Page<Produkt> findByGruppeKategorieIdAndEan(Integer kategorieId, String ean, Pageable pagination);
 
     Page<Produkt> findByGruppeKategorieId(Integer kategorieId, Pageable pagination);
 
-    Page<Produkt> findByGruppeKategorieIdAndGruppeIdAndEan(Integer kategorieId, Integer gruppeId, String ean, Pageable pagination);
-
     Page<Produkt> findByGruppeKategorieIdAndGruppeId(Integer kategorieId, Integer gruppeId, Pageable pagination);
-
-    Page<Produkt> findByGruppeIdAndEan(Integer gruppeId, String ean, Pageable pagination);
 
     Page<Produkt> findByGruppeId(Integer gruppeId, Pageable pagination);
 
