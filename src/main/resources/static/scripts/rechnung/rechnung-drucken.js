f0eb71377bafee790508d66c312937d1dcc580a2
razilein <soul_reaverfan@hotmail.de>
2019-04-26 19:35:13 +0200

    28 - Korrekte Berechnung der MwSt und des Netto-Betrags auf der Rechnung

--- src/main/resources/static/scripts/rechnung/rechnung-drucken.js
+++ src/main/resources/static/scripts/rechnung/rechnung-drucken.js
@@ -52,8 +52,8 @@ var vm = new Vue({
         vm.gesamtsumme = vm.gesamtsumme + element.gesamt;
         vm.gesamtrabatt = vm.gesamtrabatt + element.rabatt;
       });
-      vm.gesamtmwst = vm.gesamtsumme * 19 / 100;
-      vm.gesamtnetto = vm.gesamtsumme - vm.gesamtmwst;
+      vm.gesamtnetto = vm.gesamtsumme / 1.19;
+      vm.gesamtmwst = vm.gesamtsumme - vm.gesamtnetto;
       vm.entity.rechnung.datum = formatDate(vm.entity.rechnung.datum);
     },
     
