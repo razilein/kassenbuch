cfac6ec4fa5710abb641b123028d78a8cda138d8
razilein <soul_reaverfan@hotmail.de>
2020-01-21 20:29:00 +0100

    Rechnungsnummer auf Rechnungen mehr hervorheben, Auftrags-/Bestellnummern weniger hervorheben Informationen bei Überweisungen um Verwendungszweck erweitert

--- src/main/resources/de/sg/computerinsel/db/skripte/35.reparatur_bestellung.sql
+++ src/main/resources/de/sg/computerinsel/db/skripte/35.reparatur_bestellung.sql
@@ -1,2 +1,3 @@
 ALTER TABLE reparatur ADD COLUMN bestellung_id INTEGER NULL;
 ALTER TABLE reparatur ADD FOREIGN KEY (bestellung_id) REFERENCES bestellung(id) ON DELETE SET NULL;
+UPDATE reparatur SET kostenvoranschlag = '-' WHERE kostenvoranschlag IS NULL;
