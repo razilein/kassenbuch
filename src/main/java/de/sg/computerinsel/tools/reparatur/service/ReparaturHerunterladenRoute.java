7ac9e41175230da40266ec984290223aa28e169a
razilein <soul_reaverfan@hotmail.de>
2021-05-13 08:07:35 +0200

    Validierung Cron-Ausdruck beim Starten der Route zum Herunterladen von Reparaturaufträgen

--- src/main/java/de/sg/computerinsel/tools/reparatur/service/ReparaturHerunterladenRoute.java
+++ src/main/java/de/sg/computerinsel/tools/reparatur/service/ReparaturHerunterladenRoute.java
@@ -5,13 +5,16 @@ import java.io.File;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Service;
 
 import de.sg.computerinsel.tools.service.EinstellungenService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class ReparaturHerunterladenRoute extends RouteBuilder {
 
     private final EinstellungenService einstellungService;
@@ -20,6 +23,12 @@ public class ReparaturHerunterladenRoute extends RouteBuilder {
     public void configure() throws Exception {
         final String cron = RegExUtils.replaceAll(einstellungService.getRoboterCron().getWert(), StringUtils.SPACE, "+");
         if (StringUtils.isNotBlank(cron) && StringUtils.isNotBlank(einstellungService.getFtpHost().getWert())) {
+            try {
+                CronExpression.parse(cron);
+            } catch (final IllegalArgumentException e) {
+                log.error(e.getMessage(), e);
+                return;
+            }
             final String hostname = einstellungService.getFtpHost().getWert();
             final String port = einstellungService.getFtpPort().getWert();
             final String username = einstellungService.getFtpUser().getWert();
